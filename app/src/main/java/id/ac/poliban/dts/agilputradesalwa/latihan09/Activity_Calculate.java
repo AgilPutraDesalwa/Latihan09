e5c24debb8a21fd4a99f07330567b2885a3b3972
AgilPutraDesalwa <agilrsssa@gmail.com>
2019-10-24 23:38:22 -0700

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/dts/agilputradesalwa/latihan09/Activity_Calculate.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.dts.agilputradesalwa.latihan09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Calculate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__calculate);
+    }
+}
